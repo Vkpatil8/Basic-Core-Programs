d0aeeab5c425d9411ae0e5254c57ce14d9a32ec4
Vishal Kakaso Patil <59137010+Vkpatil8@users.noreply.github.com>
2021-12-10 22:20:54 +0530

    File added

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year you want to check  ");
+        int year = scanner.nextInt();
+        if (year <= 9999 && year >999){
+            if (year % 4 == 0){
+                System.out.println( year + " is leap year");
+            }
+            else{
+                System.out.println( year + " is not a leap year");
+            }
+        }
+        else {
+            System.out.println("This is for only Four digit years so plz Enter Four digit year");
+        }
+    }
+
+}
